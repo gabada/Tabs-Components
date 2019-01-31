7a9058025bbe6769cc9c6e0783c751cf000453be
Gill Abada <gill@callifi.com>
2019-01-31 17:01:18 -0500

    change item element to use custom data.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,7 +8,7 @@ class TabLink {
     this.data = element.dataset;
 
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`.tabs-item[data-tab ='${element.dataset.tab}']`);
+    this.itemElement = document.querySelector(`.tabs-item[data-tab ='${this.data.tab}']`);
 
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
